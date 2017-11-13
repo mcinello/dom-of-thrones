90960bcb7e51989a1c13dd7d9074ace7a16328a6
Michelle Cinello <m.cinello@gmail.com>
2017-11-13 16:52:00 -0500

    setup setting

---
+++ js/scene2.js
@@ -0,0 +1,39 @@
+function scene2() {
+
+  // Setup Stage
+  var stage = document.querySelector('#stage');
+  document.querySelector('#sets');
+  var castle = document.querySelector('#castle');
+  var stageImg = stage.querySelector('#stage-img');
+  var castleImgSrc = castle.getAttribute('src');
+  var stageImg = stage.querySelector('#stage-img');
+  stageImg.setAttribute('src', castleImgSrc);
+
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
